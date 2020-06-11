80dcb406ebdc0f4dd216fd31ee3752a563ebdcea
Nate Jones <nate@endot.org>
2020-06-10 19:13:27 -0700

    Tests pass!

--- la-meetup-june-2020/src/asn.clj
+++ la-meetup-june-2020/src/asn.clj
@@ -1,6 +1,25 @@
-(ns asn
-  )
+(ns asn)
+
+#_(->> (partition 2 "232425")
+       (map #(apply str %))
+       (map #(Integer. %))
+       (apply <))
+
+(defn check
+  [input size]
+  (let [sequence (->> (partition-all size input)
+                      (map #(apply str %))
+                      (map #(Integer. %)))]
+    (= sequence
+       (range (first sequence) (inc (last sequence))))))
+
+#_(check "232425" 2)
 
 (defn ascending
   [input]
-  true)
+  (->> (range 1 (inc (/ (count input) 2)))
+       (map #(check input %))
+       (some true?)))
+
+#_(ascending "232424")
+#_(ascending "2324256")
