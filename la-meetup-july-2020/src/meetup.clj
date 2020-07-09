ef75c4f789d14df77aa499fadfcdcea8407f5b8e
Dorab Patel <dorab@matchcraft.com>
2020-07-08 20:32:12 -0700

    After the meetup on quines.

--- la-meetup-july-2020/src/meetup.clj
+++ la-meetup-july-2020/src/meetup.clj
@@ -1,1 +1,8 @@
 (ns meetup)
+
+((fn [x] (list x (list 'quote x)))
+ '(fn [x] (list x (list 'quote x))))
+
+
+
+((fn [x] (println (format "(%s %s)" x x))) "(fn [x] (println (format \"(%s %s) \" x x)))")
