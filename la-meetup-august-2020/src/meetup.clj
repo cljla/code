ead39a73e260e459ba875bd0600d8f5a87329a0a
Ticean Bennett <ticean@gmail.com>
2020-08-12 20:08:20 -0700

    Work in progress

--- la-meetup-august-2020/src/meetup.clj
+++ la-meetup-august-2020/src/meetup.clj
@@ -1,8 +1,7 @@
 (ns meetup)
 
 (def digit-prefix
-  [
-   "hena"
+  ["hena"
    "do"
    "tria"
    "tetra"
@@ -10,12 +9,10 @@
    "hexa"
    "hepta"
    "octa"
-   "ennea"
-   ])
+   "ennea"])
 
 (def place-prefix
-  [
-   ""
+  [""
    "conta"
    "hecta"
    "chilia"
@@ -23,17 +20,29 @@
    "decamyria"
    "mega"
    "giga"
-   "tera"
-   ])
+   "tera"])
+
+(defn lookup-prefix [index digit]
+  (let [d (-> digit str (Integer/parseInt) dec)
+        value (str (get digit-prefix d) (get place-prefix index))]
+    (print value)
+    value))
+
+(defn polygon-name* [n]
+  (let [reversed (into [] (-> n str reverse))]
+    (str (apply str (reverse (map-indexed lookup-prefix reversed)))
+         "gon")))
 
 (defn polygon-name
   [n]
   (case n
+    1 "henagon"
+    2 "digon"
     3 "triangle"
     4 "quadrilateral"
-    )
-  )
+    9 "nonagon"
+    10 "decagon"
+    11 "hendecagon"
+    (polygon-name* n)))
 
 (def n "13493")
-
-(into [] (reverse n))
