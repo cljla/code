0e690c25bf80c8879d184166e6104cfd13c063f7
Dorab Patel <dorab@matchcraft.com>
2020-08-12 19:19:53 -0700

    WIP

--- la-meetup-august-2020/src/meetup.clj
+++ la-meetup-august-2020/src/meetup.clj
@@ -1,1 +1,39 @@
 (ns meetup)
+
+(def digit-prefix
+  [
+   "hena"
+   "do"
+   "tria"
+   "tetra"
+   "penta"
+   "hexa"
+   "hepta"
+   "octa"
+   "ennea"
+   ])
+
+(def place-prefix
+  [
+   ""
+   "conta"
+   "hecta"
+   "chilia"
+   "myria"
+   "decamyria"
+   "mega"
+   "giga"
+   "tera"
+   ])
+
+(defn polygon-name
+  [n]
+  (case n
+    3 "triangle"
+    4 "quadrilateral"
+    )
+  )
+
+(def n "13493")
+
+(into [] (reverse n))
