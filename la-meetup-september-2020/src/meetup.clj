b1fcf5a966e428c136e956f3efc6d30b8c530298
Dorab Patel <dorabpatel@gmail.com>
2020-09-10 14:07:55 -0700

    Dorab's version of the cleave code.

--- la-meetup-september-2020/src/meetup.clj
+++ la-meetup-september-2020/src/meetup.clj
@@ -2,7 +2,9 @@
   (:require
     [clojure.string :as str]))
 
-(defn find-matches
+(defn find-prefixes
+  "Returns a seq of words from `words`
+  that are at the start of the string `s`."
   [s words]
   (seq (filter #(str/starts-with? s %) words)))
 
@@ -10,25 +12,32 @@
 #_(find-matches "onetwothree" ["two" "three"])
 
 
-(declare cleave)
+(declare cleave-1)
 
 (defn cleave*
-  [match s words]
-  (let [remaining (subs s (count match))]
+  "Given a particular `prefix` of string `s`
+  known to be in `words`, returns a seq of seqs of strings."
+  [prefix s words]
+  (let [remaining (subs s (count prefix))]
     (if (seq remaining)
-      (when-let [others (cleave remaining words)]
-        (doall (map #(str match " " %) others)))
-      match)))
+      (when-let [others (cleave-1 remaining words)]
+        (seq (map #(cons prefix (first %)) others)))
+      (cons (cons prefix nil) nil))))
 
 (defn cleave-1
+  "Like cleave, but returns a seq of seqs of strings.
+  nil on failure."
   [s words]
-  (when-let [matches (find-matches s words)]
-    (println matches)
-    (doall (map #(cleave* % s words) matches))))
+  (when-let [prefixes (find-prefixes s words)]
+    (seq (remove nil? (map #(cleave* % s words) prefixes)))))
 
 (defn cleave
+  "Cleave the string `s` that does not contain any spaces,
+  with the words in `words`."
   [s words]
-  (cleave-1 s words))
+  (if-let [ret (cleave-1 s words)]
+    (str/join " " (first (first ret)))
+    "Cleaving stalled: Word not found"))
 
 
 #_(apply max-key count ["o" "one"])
