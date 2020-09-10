a864ecd041c3b6e0fa5bf5bf354dd1620fda8b3b
Nate Jones <nate@endot.org>
2020-09-09 18:53:31 -0700

    Tests

--- la-meetup-september-2020/src/meetup.clj
+++ la-meetup-september-2020/src/meetup.clj
@@ -1,1 +1,7 @@
-(ns meetup)
+(ns meetup
+  (:require
+    [clojure.string :as str]))
+
+
+(defn cleave
+  [s words])
--- la-meetup-september-2020/test/meetup_test.clj
+++ la-meetup-september-2020/test/meetup_test.clj
@@ -10,3 +10,36 @@
     (is (= 2
            (+ 1 1))))
   )
+
+(def words ["a" "after" "all" "an" "and" "are" "as" "by" "continued"
+            "deadlines" "doubly" "fish" "for" "go" "happen" "happened" "i"
+            "illusion" "is" "long" "love" "lunchtime" "make" "moment" "noise"
+            "nothing" "of" "or" "people" "problem" "second" "so" "summarize"
+            "summary" "thanks" "the" "then" "they" "time" "to" "whooshing"])
+
+(def s1 "solongandthanksforallthefish")
+(def s2 "solongandthanksforalllthefish")
+(def s3 "tosummarizethesummaryofthesummarypeopleareaproblem")
+(def s4 "timeisanillusionlunchtimedoublyso")
+(def s5 "ilovedeadlinesilovethewhooshingnoisetheymakeastheygoby")
+(def s6 "ilovedeadlinesilovethewheezingnoisetheymakeastheygoby")
+(def s7 "foramomentnothinghappenedthenafterasecondorsonothingcontinuedtohappen")
+
+
+(deftest main
+  (testing "main"
+    (is (= "so long and thanks for all the fish"
+           (cleave s1 words)))
+    (is (= "Cleaving stalled: Word not found"
+           (cleave s2 words)))
+    (is (= "to summarize the summary of the summary people are a problem"
+           (cleave s3 words)))
+    (is (= "time is an illusion lunchtime doubly so"
+           (cleave s4 words)))
+    (is (= "i love deadlines i love the whooshing noise they make as they go by"
+           (cleave s5 words)))
+    (is (= "Cleaving stalled: Word not found"
+           (cleave s6 words)))
+    (is (= "for a moment nothing happened then after a second or so nothing continued to happen"
+           (cleave s7 words)))
+    ))
