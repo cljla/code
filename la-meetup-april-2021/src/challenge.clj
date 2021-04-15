0cce93361f4e146dfa596be3890d8a45b0ae90c5
Ticean Bennett <ticean.bennett@mailchimp.com>
2021-04-14 22:06:38 -0400

    it works

--- la-meetup-april-2021/src/challenge.clj
+++ la-meetup-april-2021/src/challenge.clj
@@ -1,2 +1,16 @@
-(ns challenge)
+(ns challenge
+  (:require
+   [clojure.string :as str]))
 
+(defn palindrome? [word]
+  (= word (str/reverse word)))
+
+(defn helper [word i]
+  (let [prefix (subs word 0 i)
+        new-word (str word (str/reverse prefix))]
+    (if (palindrome? new-word)
+      i
+      (recur word (inc i)))))
+
+(defn min-palindrome-steps [word]
+  (helper word 0))
--- la-meetup-april-2021/test/challenge_test.clj
+++ la-meetup-april-2021/test/challenge_test.clj
@@ -1,10 +1,20 @@
 (ns challenge-test
   (:require
-    [clojure.test :refer :all]
-    [challenge :refer :all]))
+    [clojure.test :refer [deftest testing is]]
+    [challenge :refer [min-palindrome-steps]]))
 
 
-(deftest {{challenge}}-test
-  (testing "{{challenge}}"
-
-    ))
+(deftest challenge-test
+  (testing "challenge"
+    (is (= (min-palindrome-steps "race") 3))
+    (is (= (min-palindrome-steps "mada") 1))
+    (is (= (min-palindrome-steps "mirror") 3))
+    (is (= (min-palindrome-steps "maa") 1))
+    (is (= (min-palindrome-steps "m") 0))
+    (is (= (min-palindrome-steps "rad") 2))
+    (is (= (min-palindrome-steps "madam") 0))
+    (is (= (min-palindrome-steps "radar") 0))
+    (is (= (min-palindrome-steps "www") 0))
+    (is (= (min-palindrome-steps "me") 1))
+    (is (= (min-palindrome-steps "rorr") 2))
+    (is (= (min-palindrome-steps "pole") 3))))
