2f2a625320400e769297d15fddfefc8b71db5ee6
Aram Zadikian <az@whatisfood.com>
2021-05-12 19:28:56 -0700

    solved

--- la-meetup-may-2021/src/challenge.clj
+++ la-meetup-may-2021/src/challenge.clj
@@ -1,2 +1,60 @@
 (ns challenge)
 
+(defn is-even-digits
+  [n]
+  (even? (count (str n))))
+
+(defn num->vec
+  [n]
+  (-> n
+      str
+      seq
+      vec))
+
+(defn permutations [v-coll]
+  (if (<= (count v-coll) 1)
+    (list v-coll)
+    (mapcat (fn [i]
+              (let [elm-0 (get v-coll 0)
+                    elm-i (get v-coll i)
+                    sub-v-coll (-> v-coll
+                                   (assoc i elm-0)
+                                   (subvec 1))]
+                (for [sub-permutations (permutations sub-v-coll)]
+                  (into [elm-i] sub-permutations))))
+            (range (count v-coll)))))
+
+(defn mul-char-pairs
+  [a b]
+  (->> (map (fn [chars]
+              (Integer. (apply str chars))) [a b])
+       (apply *)))
+
+(defn check-perm
+  [coll]
+  (let [len (/ (count coll) 2)
+        a (take len coll)
+        b (nthrest coll len)]
+    (mul-char-pairs a b)))
+
+(defn vampirish
+  [n]
+  (->> n
+       num->vec
+       permutations
+       (map check-perm)
+       (some #(= n %))))
+
+(defn isVampire
+  [n]
+  (let [is-even (is-even-digits n)]
+    (cond
+      (< n 100) "Normal Number"
+
+      (and is-even
+           (vampirish n)) "True Vampire"
+
+      (and (not is-even)
+           (vampirish n)) "Pseudovampire"
+
+      :else "Normal Number")))
--- la-meetup-may-2021/test/challenge_test.clj
+++ la-meetup-may-2021/test/challenge_test.clj
@@ -1,10 +1,21 @@
 (ns challenge-test
   (:require
-    [clojure.test :refer :all]
-    [challenge :refer :all]))
+   [clojure.test :refer [deftest testing is]]
+   [challenge :refer [isVampire]]))
 
 
-(deftest {{challenge}}-test
-  (testing "{{challenge}}"
+(deftest challenge-test
+  (testing "challenge"
+    (is (= "True Vampire" (isVampire 1260)))
+    (is (= "Pseudovampire" (isVampire 126)))
+    (is (= "Normal Number" (isVampire 67)))
+    (is (= "Normal Number" (isVampire 1)))
+    (is (= "Normal Number" (isVampire 645)))
+    (is (= "Pseudovampire" (isVampire 688)))
+    (is (= "Normal Number" (isVampire 1345)))
+    (is (= "True Vampire" (isVampire 1395)))
+    (is (= "Pseudovampire" (isVampire 12964)))
+    (is (= "Normal Number" (isVampire 98765)))
+    (is (= "Normal Number" (isVampire 124421)))
+    (is (= "True Vampire" (isVampire 125460)))))
 
-    ))
