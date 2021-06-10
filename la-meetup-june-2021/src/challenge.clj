88feee42c0f1fe7d6d295994d85e90c01e57e85e
Nate Jones <nate@endot.org>
2021-06-09 19:51:00 -0700

    June works!

--- la-meetup-june-2021/src/challenge.clj
+++ la-meetup-june-2021/src/challenge.clj
@@ -1,2 +1,21 @@
 (ns challenge)
 
+(defn prime?
+  [n]
+  (every? #(not (zero? (rem n %))) (range 2 (inc (Math/sqrt n)))))
+
+(defn prime-gaps
+  [g a b]
+  (loop [last-prime nil
+         nums (range a (inc b))]
+    (when nums
+      (let [[num & remaining] nums]
+        (cond
+          (not (prime? num))
+          (recur last-prime remaining)
+
+          (and last-prime (= g (- num last-prime)))
+          [last-prime num]
+
+          :else
+          (recur num remaining))))))
--- la-meetup-june-2021/test/challenge_test.clj
+++ la-meetup-june-2021/test/challenge_test.clj
@@ -1,10 +1,19 @@
 (ns challenge-test
   (:require
-    [clojure.test :refer :all]
-    [challenge :refer :all]))
+    [clojure.test :refer [deftest testing is]]
+    [challenge :refer [prime-gaps]]))
 
 
-(deftest {{challenge}}-test
-  (testing "{{challenge}}"
-
+(deftest prime-gaps-test
+  (testing "Prime gaps"
+    (is (= [3 5] (prime-gaps 2 3 50))) ;; failed
+    (is (= [5 7] (prime-gaps 2 5 7)))
+    (is (= nil (prime-gaps 2 5 5)))
+    (is (= [163 167] (prime-gaps 4 130 200)))
+    (is (= [101 103] (prime-gaps 2 100 110)))
+    (is (= [103 107] (prime-gaps 4 100 110)))
+    (is (= nil (prime-gaps 6 100 110)))
+    (is (= [359 367] (prime-gaps 8 300 400))) ;; failed
+    (is (= [337 347] (prime-gaps 10 300 400)))
+    (is (= nil (prime-gaps 11 30000 100000)))
     ))
